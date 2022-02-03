f30c3256dff715778ba2e82c740a7ce0e05fee77
DrDano <danielharned@gmail.com>
2022-02-02 20:09:03 -0800

    adding post routes and connecting routes to main app through index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+
+router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
+
 module.exports = router;
