2563b18af6d7d0de16782427d3ba8d8251110515
DrDano <danielharned@gmail.com>
2022-02-08 14:13:38 -0800

    fixing issue in public/javascript/upvote.js that was making the post_id undefined

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -35,7 +35,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
       id: req.params.id
@@ -90,13 +90,15 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
+  // make sure the session exists first
   if (req.session) {
+    // pass session id along with all destructured properties on req.body
     Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   }
 });
 
